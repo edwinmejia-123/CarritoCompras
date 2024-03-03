7ad0e94221c6b746212911c2bce5095762a1794a
edwinmejia-123 <edwinmejia-123>
2024-03-02 22:17:19 -0600

    Creacion del esqueleto del proyecto

--- src/Main.kt
+++ src/Main.kt
@@ -26,3 +26,51 @@ fun main() {
         }
     }
 }
+
+fun mostrarProductos(productos: List<Producto>, scanner: Scanner) {
+    var regresar = false
+    while (!regresar) {
+        println("\nProductos disponibles:")
+
+        println("\n****************************************")
+        println("**** 1. Agregar producto al carrito ****")
+        println("**** 2. Regresar al menú principal  ****")
+        println("**** 3. Salir                       ****")
+        println("***************************************")
+        print("Seleccione una opción: ")
+        when (scanner.nextInt()) {
+            1 -> println("Funcion agregar producto al carrito")
+            2 -> regresar = true
+            3 -> {
+                println("Gracias por usar la aplicación.")
+                System.exit(0)
+            }
+            else -> println("Opción no válida. Intente de nuevo.")
+        }
+    }
+}
+
+fun mostrarCarrito( scanner: Scanner) {
+    var regresar = false
+    while (!regresar) {
+        println("\nProductos seleccionados:")
+        //Mostrar la lista de productos del carrito
+        println("\n***************************************")
+        println("****1. Editar cantidad de producto ****")
+        println("****2. Eliminar producto             ****")
+        println("****3. Regresar al menú principal    ****")
+        println("****4. Salir                         ****")
+        println("***************************************")
+        print("Seleccione una opción: ")
+        when (scanner.nextInt()) {
+            1 -> println("Editar cantidad de producto")
+            2 -> println("Eliminar producto")
+            3 -> regresar = true
+            4 -> {
+                println("Gracias por usar la aplicación.")
+                System.exit(0)
+            }
+            else -> println("Opción no válida. Intente de nuevo.")
+        }
+    }
+}
