4a1d0ed44d2c120b42b3eb2fbfb1aa5764af455e
edwinmejia-123 <edwinmejia-123>
2024-03-03 20:42:58 -0600

    Creaciacion e implementacion de la funcion Agregar y eliminar producto del carrito

--- src/Carrito.kt
+++ src/Carrito.kt
@@ -1,6 +1,33 @@
 
 class Carrito(val productos: MutableList<Producto> = mutableListOf()) {
-  fun mostrarCarrito() {
+    fun agregarProducto(producto: Producto, cantidad: Int) {
+        if (cantidad <= 0) {
+            println("La cantidad debe ser mayor a 0.")
+            return
+        }
+        if (producto.cantidadDisponible < cantidad) {
+            println("No hay suficiente cantidad disponible del producto ${producto.nombre}.")
+            return
+        }
+        val indiceProducto = productos.indexOfFirst { it.nombre == producto.nombre }
+        if (indiceProducto == -1) {
+            productos.add(producto.copy(cantidadDisponible = cantidad))
+        } else {
+            productos[indiceProducto] = productos[indiceProducto].copy(cantidadDisponible = productos[indiceProducto].cantidadDisponible + cantidad)
+        }
+        println("Producto ${producto.nombre} agregado al carrito.")
+    }
+
+    fun eliminarProducto(nombreProducto: String) {
+        val indiceProducto = productos.indexOfFirst { it.nombre == nombreProducto }
+        if (indiceProducto == -1) {
+            println("No se encontró el producto $nombreProducto en el carrito.")
+            return
+        }
+        productos.removeAt(indiceProducto)
+        println("Producto $nombreProducto eliminado del carrito.")
+    }
+    fun mostrarCarrito() {
         if (productos.isEmpty()) {
             println("El carrito está vacío.")
             return
